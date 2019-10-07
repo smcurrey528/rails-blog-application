76bcd3608ee61c3433b313394ab5356effa650dc
sarahcurrey <sarah.currey@generalassemb.ly>
2019-10-07 10:48:54 -0400

    Add Views Index Controller Method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,22 @@
 class ArticlesController < ApplicationController
 
-def new
-    @article = Article.new(params.require(:article).permit(:title, :author, :text))
+    def new
+        @article = Article.new
+    end
 
-    @article.save
-    redirect_to @article
+    def create
+        @article = Article.new(article_params)
+
+        @article.save
+        redirect_to @article
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
+    end
+
 end
