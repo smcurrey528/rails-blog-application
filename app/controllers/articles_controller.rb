7f3c8740e8249f6b9ce82e6d41cabc9548a89a77
sarahcurrey <sarah.currey@generalassemb.ly>
2019-10-07 09:48:38 -0400

    Create DB Migration File

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 
 def new
-    @article = Article.new
+    @article = Article.new(params.require(:article).permit(:title, :author, :text))
+
+    @article.save
+    redirect_to @article
 end
